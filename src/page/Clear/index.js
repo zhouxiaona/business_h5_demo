e3f672967230095d7f2dfb0bf8204456e8d769cf
zhouna <zna1121@163.com>
2020-08-26 15:36:40 +0800

    修改清除缓存

--- src/page/Clear/index.js
+++ src/page/Clear/index.js
@@ -3,7 +3,7 @@ import { Cookies } from '../../type/index.d';
 
 class Clear extends React.Component {
     componentWillMount() {
-        Cookies.set('userdata', undefined, { expires: 3 });
+        Cookies.remove("userdata")
         sessionStorage.clear()
         localStorage.clear()
         window.location.href = window.location.origin + "/"
