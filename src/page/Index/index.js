ebb3bf892d8b6d1a56b24254d570322cd47acb33
zhouna <nana.zhou@yeehagames.com>
2022-11-27 15:54:41 +0800

    test-commit-2

--- src/page/Index/index.js
+++ src/page/Index/index.js
@@ -29,6 +29,7 @@ class Index extends React.Component {
         console.log('this is a test dev branch commit22')
         console.log('this is a test dev2 branch commit33')
         // test cimmit-1
+        // test cimmit-2
         let { targetNumber, targetNumberArr } = this.state
         let arr = (targetNumber + '').split('')
         arr = arr.map(item => item = Number(item))
